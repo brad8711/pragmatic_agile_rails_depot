1f87d57307f806b2dfc599ee6e4d2f7d6048c130
Bradly Swart <brad8711@gmail.com>
2014-01-30 17:08:22 +1100

    leanred about validation,fixtures and testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: {greater_that_or_equal_to: 0.01}
+  validates :image_url,
+              allow_blank: true,
+              format: {
+                with: %r{\.(gif|jpg|jpeg|png)\Z}i,
+                message: 'Image URL must be .gif, .png or .jpg' 
+              }
 end
