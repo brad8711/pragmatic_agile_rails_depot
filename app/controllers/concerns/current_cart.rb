527087585af33d3fd58353fbc109d01e3f052ffa
Bradly Swart <brad8711@gmail.com>
2014-03-18 16:59:21 +1100

    added cart model, and a new concern for setting the user's cart based on the session[:card_id] if one exists

---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,13 @@
+module CurrentCart
+  extend ActiveSupport::Concern
+
+  private
+
+    def set_cart
+      @cart = Cart.find(session[:card_id])
+    rescue ActiveRecord::RecordNotFound
+      @cart = Cart.create
+      session[:card_id] = @cart.idd
+    end
+
+end
