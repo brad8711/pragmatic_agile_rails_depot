5390ba4cc4656e008c414f4ae389870dffc16454
Bradly Swart <brad8711@gmail.com>
2014-03-18 16:53:56 +1100

    iteration C5

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,10 @@ class Product < ActiveRecord::Base
               allow_blank: true,
               format: {
                 with: %r{\.(gif|jpg|jpeg|png)\Z}i,
-                message: 'Image URL must be .gif, .png or .jpg' 
+                message: 'Image URL must be .gif, .png or .jpg'
               }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
