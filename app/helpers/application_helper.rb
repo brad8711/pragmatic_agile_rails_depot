f1a0a895d3f70786f6eeee8e3ccee28e84b09ad8
Bradly Swart <brad8711@gmail.com>
2014-03-22 14:52:37 +1100

    created a helper to hide the cart in the side bar if it is empty, and animate it when a line item is added to the cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes['style'] = 'display: none'
+    end
+      #content tag helper is used to wrap the output generate by the block in a tag
+      content_tag("div", attributes, &block)
+  end
+
 end
